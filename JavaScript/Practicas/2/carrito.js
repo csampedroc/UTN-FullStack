e6e79bb666b1dfa1fea2061603e45cacac64e770
csampedroc <csampedroc@gmail.com>
2021-01-18 12:33:26 -0500

    Carrito de compras estilos y funcionalidades

---
+++ JavaScript/Practicas/2/carrito.js
@@ -0,0 +1,128 @@
+
+var productos =  [
+
+    {
+        nombre: "harina",
+        precio: 35
+    },
+
+    {
+        nombre: "pan",
+        precio: 25
+    },
+
+    {
+        nombre: "papa",
+        precio: 52
+    },
+
+    {
+        nombre: "palta",
+        precio: 55
+    },    
+
+    {
+        nombre: "fideos",
+        precio: 85
+    },
+
+    {
+        nombre: "aceite",
+        precio: 350
+    },
+
+    {
+        nombre: "sopa",
+        precio: 86
+    },
+
+    {
+        nombre: "mermelada",
+        precio: 108
+    },
+
+    {
+        nombre: "porotos",
+        precio: 69
+    },
+
+    {
+        nombre: "lentejas",
+        precio: 85
+    },
+
+    {
+        nombre: "mandarina",
+        precio: 43
+    },
+
+    {
+        nombre: "banana",
+        precio: 79
+    },    
+
+    {
+        nombre: "leche de almendras",
+        precio: 145
+    },
+
+    {
+        nombre: "papel higienico",
+        precio: 147
+    },
+
+    {
+        nombre: "lavandina",
+        precio: 55
+    },
+
+    {
+        nombre: "alcohol en gel",
+        precio: 123
+    },    
+
+    {
+        nombre: "shampoo",
+        precio: 400
+    },
+
+    {
+        nombre: "harina",
+        precio: 35
+    },
+
+    {
+        nombre: "salsa de tomate",
+        precio: 35
+    }
+
+];
+
+let tabla = document.getElementById("articulos");
+
+for(let i = 0; i < productos.length ; i++) {
+
+    let fila = document.createElement("tr");
+
+    let columnaNombre = document.createElement("td");
+    let valorNombre = document.createTextNode(productos[i].nombre);
+    
+    columnaNombre.appendChild(valorNombre);
+    
+    let columnaPrecio = document.createElement("td");
+    let valorPrecio = document.createTextNode(productos[i].precio);
+    
+    columnaPrecio.appendChild(valorPrecio);
+    
+    fila.appendChild(columnaNombre);
+    fila.appendChild(columnaPrecio)
+
+    fila.onclick=agregarProducto;
+    
+    tabla.appendChild(fila);
+
+}
+    
+function agregarProducto {
+    console.log("Pulso clic ");
+}
